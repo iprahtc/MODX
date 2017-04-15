bae606b46a77c53f76dcef1dedbbf11cb3e3dd57
iprahtc <ipra-8@mail.ru>
2017-04-15 21:20:18 +0300

    Перенес в assets содержимое папки ilya

---
+++ assets/cache/docid_1.pageCache.php
@@ -0,0 +1,9 @@
+<?php die('Unauthorized access.'); ?>a:43:{s:2:"id";s:1:"1";s:4:"type";s:8:"document";s:11:"contentType";s:9:"text/html";s:9:"pagetitle";s:4:"Home";s:9:"longtitle";s:0:"";s:11:"description";s:0:"";s:5:"alias";s:5:"index";s:15:"link_attributes";s:0:"";s:9:"published";s:1:"1";s:8:"pub_date";s:1:"0";s:10:"unpub_date";s:1:"0";s:6:"parent";s:1:"0";s:8:"isfolder";s:1:"0";s:9:"introtext";s:0:"";s:7:"content";s:0:"";s:8:"richtext";s:1:"1";s:8:"template";s:1:"3";s:9:"menuindex";s:1:"0";s:10:"searchable";s:1:"1";s:9:"cacheable";s:1:"1";s:9:"createdby";s:1:"1";s:9:"createdon";s:10:"1130304721";s:8:"editedby";s:1:"1";s:8:"editedon";s:10:"1130304927";s:7:"deleted";s:1:"0";s:9:"deletedon";s:1:"0";s:9:"deletedby";s:1:"0";s:11:"publishedon";s:10:"1130304721";s:11:"publishedby";s:1:"1";s:9:"menutitle";s:0:"";s:7:"donthit";s:1:"0";s:11:"haskeywords";s:1:"0";s:11:"hasmetatags";s:1:"0";s:10:"privateweb";s:1:"0";s:10:"privatemgr";s:1:"0";s:13:"content_dispo";s:1:"0";s:8:"hidemenu";s:1:"0";s:13:"alias_visible";s:1:"1";s:4:"desc";a:5:{i:0;s:4:"desc";i:1;s:13:"[*introtext*]";i:2;s:0:"";i:3;s:0:"";i:4;s:8:"textarea";}s:4:"keyw";a:5:{i:0;s:4:"keyw";i:1;s:13:"[*pagetitle*]";i:2;s:0:"";i:3;s:0:"";i:4;s:4:"text";}s:7:"noIndex";a:5:{i:0;s:7:"noIndex";i:1;s:0:"";i:2;s:0:"";i:3;s:0:"";i:4;s:8:"checkbox";}s:4:"titl";a:5:{i:0;s:4:"titl";i:1;s:29:"[*pagetitle*] - [(site_name)]";i:2;s:0:"";i:3;s:0:"";i:4;s:4:"text";}s:17:"__MODxDocGroups__";s:0:"";}<!--__MODxCacheSpliter__--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<title>Home - MODX Site By Dmi3yy</title>
+
+<meta name="keywords" content="Home" />
+<meta name="description" content="" />
+<base href="http://modx.ua/"/>
+
+
+<!--  Mem : [^m^], MySQL: [^qt^], [^q^] request(s), PHP: [^p^], total: [^t^], document from [^s^]. -->
