5231013efbeeffda029837ddea63254d25c8e5a8
iprahtc <ipra-8@mail.ru>
2017-05-20 10:15:06 +0300

    перенес вызов функций js после отработки ajax

--- assets/js/wotch/form_wotch.js
+++ assets/js/wotch/form_wotch.js
@@ -6,7 +6,16 @@ function forWotch(name_site){
 			data = JSON.parse(data);
 			$(".target").html(data.target);
 			$(".time-wotch").html(data.time_wotch);
+			$(".click-firm").click();
 			$('select').material_select();
+			
+			$( ".city" ).focus(function() {
+				$("#city_on").hide(300);
+			});
+			$( ".city" ).focusout(function() {
+				$("#city_on").show();
+			});
 		});
 }
+
 
