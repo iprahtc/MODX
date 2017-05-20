ff4a141df0a237330f9f3ca03489fb1c73da81fa
iprahtc <ipra-8@mail.ru>
2017-05-20 18:02:20 +0300

    доработка вкладок

--- assets/js/wotch/form_wotch.js
+++ assets/js/wotch/form_wotch.js
@@ -5,17 +5,30 @@ function forWotch(name_site){
 		},function(data){
 			data = JSON.parse(data);
 			$(".target").html(data.target);
+			//console.log(data.target);
 			$(".time-wotch").html(data.time_wotch);
 			$(".click-firm").click();
 			$('select').material_select();
 			
-			$( ".city" ).focus(function() {
-				$("#city_on").hide(300);
-			});
-			$( ".city" ).focusout(function() {
+			if($("input[name='city']").val())
+				$("#city_on").hide();
+			else
 				$("#city_on").show();
+			$( ".city" ).focus(function() {
+					$("#city_on").hide(300);
 			});
 		});
+}
+
+function validationSite(){
+	var test;
+	test = $("input[name='first_name']").val();
+	if(test){
+		console.log(test);
+	}
+	else{
+		$(".wotch-site").html('Вы не заполнили в форме колонку "название товора"');
+	}
 }
 
 
