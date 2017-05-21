ea4f52eb67e9f6781bda8cd6f7d60f79d5c2b756
iprahtc <iprahtc@gmail.com>
2017-05-21 17:02:08 +0200

    доработка

--- assets/js/wotch/form_wotch.js
+++ assets/js/wotch/form_wotch.js
@@ -1,4 +1,6 @@
+var site_name;
 function forWotch(name_site){
+	window.site_name = name_site;
 	$.get("/new_wotch",
 		{
 			name: name_site
@@ -21,13 +23,64 @@ function forWotch(name_site){
 }
 
 function validationSite(){
-	var test;
-	test = $("input[name='first_name']").val();
-	if(test){
-		console.log(test);
-	}
-	else{
-		$(".wotch-site").html('Вы не заполнили в форме колонку "название товора"');
+	//console.log(site_name);
+	switch (a) {
+		case seasonvar:
+			var film = $("input[name='first_name']").val();
+			var season = $("input[name='season']").val();
+			var series = $("input[name='series']").val();
+			var translation = $("input[name='translation']").val();
+			var fullHd1080 = $("input[id='fullHd1080']").val();
+			var hd720 = $("input[id='hd720']").val();
+			var c480p = $("input[id='c480p']").val();
+			var camrip = $("input[id='camrip']").val();
+			var webdl = $("input[id='webdl']").val();
+			if(film){
+				$.get("/new_wotch",
+				{
+					name_site: site_name,
+					name_film: film,
+					season: season,
+					series: series,
+					translation: translation,
+					fullHd1080: fullHd1080,
+					hd720: hd720,
+					c480p: c480p,
+					camrip: camrip,
+					webdl: webdl
+					
+				},function(data){
+					
+				});
+			}
+			else{
+				$(".wotch-site").html('Вы не заполнили в форме колонку "название фильма"');
+			}
+			break;
+			
+		case olx:
+			var tovar = $("input[name='first_name']").val();
+			var price_start = $("input[name='price_start']").val();
+			var price_final = $("input[name='price_final']").val();
+			var region = $("input[name='region']").val();
+			var city = $("input[id='city']").val();
+			if(film){
+				$.get("/new_wotch",
+				{
+					name_site: site_name,
+					tovar: tovar,
+					price_start: price_start,
+					region: region,
+					city: city
+					
+				},function(data){
+					
+				});
+			}
+			else{
+				$(".wotch-site").html('Вы не заполнили в форме колонку "название фильма"');
+			}
+			break;	
 	}
 }
 
