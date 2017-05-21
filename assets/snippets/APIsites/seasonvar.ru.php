f094152cc4607c37db4cd6811046fbcd93c36f57
igorded1991 <igor_ded1991@mail.ru>
2017-05-21 20:15:38 +0300

    Доработка апи сайта seasonvar

--- assets/snippets/APIsites/seasonvar.ru.php
+++ assets/snippets/APIsites/seasonvar.ru.php
@@ -27,7 +27,7 @@ if($isGet) {
 		$last_season = $data[$i];
 		if(!isset($last_season['id']))
 			break;
-		$last_season['source'] = $site_use['name'];
+		$last_season['source'] = $site_use['site_name'];
 		$last_season['url'] = "http://seasonvar.ru/";
 		if($last_season['season'][0] > 0)
 			$season = ' ('.$last_season['season'][0].' сезон)';
